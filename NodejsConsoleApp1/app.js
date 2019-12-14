e0d3f46f85c70a41b53cc1fe037854c581904033
Jflynn426 <jflynnpics@gmail.com>
2019-12-14 16:17:44 -0500

    JS code katas II

--- NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/app.js
@@ -1,3 +1,17 @@
-'use strict';
-
-console.log('Hello world');
+function skiJump(mountain) {
+    jump = Math.pow(mountain.length, 2) * 1.35
+    switch (true) {
+        case (jump < 10):
+            return `${jump.toFixed(2)} metres: He's crap!`
+            break
+        case (jump < 25):
+            return `${jump.toFixed(2)} metres: He's ok!`
+            break
+        case (jump < 50):
+            return `${jump.toFixed(2)} metres: He's flying!`
+            break
+        default:
+            return `${jump.toFixed(2)} metres: Gold!!`
+            break
+    }
+}
